39fae8b4ae5eb93066487ebef71c14981d9124df
Jeel Patel <153211984+JeelDev@users.noreply.github.com>
2025-02-05 21:18:12 -0700

    Update profile.tsx

--- myCloneApp/app/(tabs)/profile.tsx
+++ myCloneApp/app/(tabs)/profile.tsx
@@ -20,7 +20,7 @@ export default function TabTwoScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+        <ThemedText type="title">Profile</ThemedText>
       </ThemedView>
     </ParallaxScrollView>
   );
